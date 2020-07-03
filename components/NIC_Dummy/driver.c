989245bcf703f86b4b956fc1e5c9db2037924b3b
Benedikt Driessen <benedikt.driessen.external@hensoldt-cyber.com>
2020-07-03 12:48:51 +0200

    [NEW] SEOS-1438: Add NIC_Dummy driver

---
+++ components/NIC_Dummy/driver.c
@@ -0,0 +1,40 @@
+/*
+ *  NIC Dummy Driver
+ *
+ *  Copyright (C) 2020, Hensoldt Cyber GmbH
+ */
+
+#include "OS_Error.h"
+#include "OS_Dataport.h"
+
+#include "LibDebug/Debug.h"
+
+#include <string.h>
+
+#include <camkes.h>
+
+static OS_Dataport_t port = OS_DATAPORT_ASSIGN(nic_port_to);
+
+OS_Error_t
+nic_rpc_tx_data(
+    size_t* pLen)
+{
+    Debug_LOG_TRACE("[NIC '%s'] %s()", get_instance_name(), __func__);
+    return OS_SUCCESS;
+}
+
+OS_Error_t
+nic_rpc_get_mac(void)
+{
+    static const uint8_t mac[6] =
+    {
+        0x00, 0x11, 0x22, 0x33, 0x44, 0x55
+    };
+
+    Debug_LOG_TRACE("[NIC '%s'] %s()", get_instance_name(), __func__);
+
+    // Copy a dummy MAC
+    memcpy(OS_Dataport_getBuf(port), mac, sizeof(mac));
+
+    return OS_SUCCESS;
+}
