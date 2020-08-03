70146e1e02757494fa9e46c9cbb89f9008b789b8
Axel Heider <axel.heider@hensoldt-cyber.com>
2020-08-03 14:29:06 +0200

    [DEV] simplify proxy connection parameter handling

--- scripts/run_qemu.sh
+++ scripts/run_qemu.sh
@@ -17,34 +17,34 @@ if [[ ! -e "${SYSTEM_IMAGE}" ]]; then
     exit 1
 fi
 
-PARAM_CON_QEMU=${1:-}
-if [[ ! -z "${1:-}" ]]; then
-    shift
-fi
-
-#-------------------------------------------------------------------------------
-# QEMU serial port 0 connection
 
 # no Proxy communication as default
 CON_QEMU_UART_PROXY="-serial /dev/null"
-
-if [[ -z "${PARAM_CON_QEMU}" ]]; then
-    echo "No QEMU connection was set."
-
-elif [[ ${PARAM_CON_QEMU} == "PTY" ]]; then
-    # QEMU connects serial port to newly created PTY, "-S" makes it freeze on
-    # startup to allow a host application to connect there
-    CON_QEMU_UART_PROXY="-S -serial pty"
-
-elif [[ ${PARAM_CON_QEMU} == "TCP" ]]; then
-    # QEMU waits on port 4444 for a connection, connects serial port to it and
-    # then starts the system
-    CON_QEMU_UART_PROXY="-serial tcp:localhost:4444,server"
-
+if [[ -z "${1:-}" ]]; then
+    echo "No QEMU/Proxy connection."
 else
-    echo "ERROR: invalid conection type: ${PARAM_CON_QEMU}"
-    exit 1
+    PARAM_CON_QEMU=${1}
+    shift
 
+    case ${PARAM_CON_QEMU} in
+        "PTY")
+            # QEMU connects serial port to newly created PTY, "-S" makes it
+            # freeze on startup to allow a host application to connect there
+            echo "QEMU/Proxy connection via PTY"
+            CON_QEMU_UART_PROXY="-S -serial pty"
+            ;;
+
+        "TCP")
+            # QEMU waits on port 4444 for a connection, connects serial port to
+            # it and then starts the system
+            echo "QEMU/Proxy connection via TCP"
+            CON_QEMU_UART_PROXY="-serial tcp:localhost:4444,server"
+            ;;
+
+        *)
+            echo "ERROR: invalid conection type: ${PARAM_CON_QEMU}"
+            exit 1
+    esac
 fi
 
 
