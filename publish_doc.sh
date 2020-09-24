7ac90876e647b2c5fa8bab242b16d63304c799f8
Thomas Böhm <thomas.boehm@hensoldt-cyber.com>
2020-09-24 17:22:46 +0200

    [FOR] Rename path from seos to trentos

--- publish_doc.sh
+++ publish_doc.sh
@@ -11,7 +11,7 @@
 DOC_ROOT=/artefacts/documentation
 
 BRANCH_NAME=$1
-DOC_DIR=${DOC_ROOT}/seos_sdk/${BRANCH_NAME}
+DOC_DIR=${DOC_ROOT}/trentos_sdk/${BRANCH_NAME}
 
 rm -rf ${DOC_DIR}
 mkdir -p ${DOC_DIR}
