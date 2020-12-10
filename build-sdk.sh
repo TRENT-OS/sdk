8e175b17af1427f8db446eff223c1e2ef400ee32
Axel Heider <axel.heider@hensoldt-cyber.de>
2020-12-10 12:01:40 +0100

    [DEV] Add demo_iot_app_imx6

--- build-sdk.sh
+++ build-sdk.sh
@@ -288,17 +288,19 @@ function build_sdk_demos()
 
     # not every demo works on all platforms
     #
-    #                      | zynq7000 | rpi3 | sabre | migv | ...
-    # ---------------------+----------+------+-------+------+-----
-    #  demo_hello_world    | yes      | yes  | yes   | yes  |
-    #  demo_iot_app        | yes      | no   | no    | no   |
-    #  demo_iot_app_rpi3   | no       | yes  | no    | no   |
-    #  demo_raspi_ethernet | no       | yes  | no    | no   |
-    #  demo_tls_api        | yes      | no   | no    | no   |
+    #                      | zynq7000 | rpi3 | sabre | migv | nitrogen6sx | ...
+    # -------------------- +----------+------+-------+------+-------------+-----
+    #  demo_hello_world    | yes      | yes  | yes   | yes  | yes         |
+    #  demo_iot_app        | yes      | no   | no    | no   | no          |
+    #  demo_iot_app_rpi3   | no       | yes  | no    | no   | no          |
+    #  demo_iot_app_imx6   | no       | no   | yes   | no   | yes         |
+    #  demo_raspi_ethernet | no       | yes  | no    | no   | no          |
+    #  demo_tls_api        | yes      | no   | no    | no   | no          |
     #
     declare -A TARGET_RESTRICTIONS=(
         [demo_iot_app]=zynq7000
         [demo_iot_app_rpi3]=rpi3
+        [demo_iot_app_imx6]="sabre nitrogen6sx"
         [demo_raspi_ethernet]=rpi3
         [demo_tls_api]=zynq7000
     )
