2f23746194398a62c1090a91c81c7b6bc590997b
Slawomir Kwasniak <slawomir.kwasniak@hensoldt-cyber.de>
2020-12-14 13:06:52 +0100

    [FOR] Remove trailing spaces check_static_analyzer

--- check_static_analyzer.sh
+++ check_static_analyzer.sh
@@ -7,9 +7,9 @@ cppcheck --project=build/compile_commands.json --output-file=cppcheck_output.txt
     -i "${WORDDIR}/src/sdk/sdk-sel4-camkes/"
 
 # CMake always builds some libraries which expect a configuration header, which isn't
-# provided by the test system if it doesn't use that library. This filters out the error 
-# messages cppcheck generates for that case. 
-excluded=( 
+# provided by the test system if it doesn't use that library. This filters out the error
+# messages cppcheck generates for that case.
+excluded=(
     \#error
 )
 
@@ -17,7 +17,7 @@ excluded=(
 echo -e "\n CPPCHECK OUTPUT \n"
 cat "cppcheck_output.txt"
 
-# remove all lines from the output file which match elements in the 
+# remove all lines from the output file which match elements in the
 # exclusion list
 
 grep -v `printf '%s '"${excluded[@]/#/-e }"` "cppcheck_output.txt" > "cppcheck_output.txt.out"
