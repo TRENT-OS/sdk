4e3479a8c4b8dc37a50938dc1c45ba8e98eb07e6
Franz Schauer <franz.schauer@hensoldt-cyber.de>
2021-03-08 11:01:50 +0100

    [DEV] execute astyle_check_sdk.sh in working directory

--- astyle_check_sdk.sh
+++ astyle_check_sdk.sh
@@ -1,40 +1,38 @@
 #!/bin/bash
 
-(
-    cd "$(dirname "$0")"
+echo "---"
+echo "Execute astyle check in:"
+echo `pwd`
+echo "-"
 
-    echo "---"
-    echo "Execute astyle check for SDK"
+# remove previously existing astyle files
+find . -name '*.astyle' -exec rm -v {} \;
 
-    # remove previously existing astyle files
-    find . -name '*.astyle' -exec rm -v {} \;
+# find and execute astyle checks
+find . -name 'astyle_check.sh' -execdir {} \;
 
-    # find and execute astyle checks
-    find . -name 'astyle_check.sh' -execdir {} \;
+# find all created astyle files
+FILES=$(find . -name '*.astyle')
 
-    # find all created astyle files
-    FILES=$(find . -name '*.astyle')
+echo "-"
 
+# check if any astyle files have been created
+if [ ! -z "${FILES}" ]; then
+    echo "ERROR: astyle issues found."
     echo "-"
+    echo "Check the following files:"
 
-    # check if any astyle files have been created
-    if [ ! -z "${FILES}" ]; then
-        echo "ERROR: astyle issues found."
-        echo "-"
-        echo "Check the following files:"
+    for FILE in ${FILES}; do
+        SRC_FILE=${FILE%.astyle} # get file name without astyle suffix
+        echo "  ${SRC_FILE}"
+    done
 
-        for FILE in ${FILES}; do
-            SRC_FILE=${FILE%.astyle} # get file name without astyle suffix
-            echo "  ${SRC_FILE}"
-        done
-
-        echo "---"
-
-        exit 1 # error
-    fi
-
-    echo "INFO: No astyle issue found."
     echo "---"
 
-    exit 0 # success
-)
+    exit 1 # error
+fi
+
+echo "INFO: No astyle issue found."
+echo "---"
+
+exit 0 # success
