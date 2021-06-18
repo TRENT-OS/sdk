17fb62d15b55d31da3bfb0c71cff70dcf5bae6d4
Franz Schauer <franz.schauer@hensoldt-cyber.de>
2021-06-18 08:30:27 +0200

    [DEV] use axivion toolchain file if ENABLE_ANALYSIS is ON

--- libs/test/analysis/axivion/build.sh
+++ libs/test/analysis/axivion/build.sh
@@ -4,6 +4,9 @@
 # Copyright (C) 2021, HENSOLDT Cyber GmbH
 #
 # Build the analysis project.
+#
+# The environment variable ENABLE_ANALYSIS has to be set to ON if the build
+# shall be executed with the Axivion Suite. Default for regular build is OFF.
 #-------------------------------------------------------------------------------
 
 # get the directory the script is located in
@@ -24,10 +27,21 @@ mkdir ${BUILD_DIR} -p
 # CMake config
 #-------------------------------------------------------------------------------
 
+ENABLE_ANALYSIS=${ENABLE_ANALYSIS:-OFF}
+
 CMAKE_PARAMS=(
     -D BUILD_ANALYSIS=ON
     -G Ninja
 )
+
+if [[ ${ENABLE_ANALYSIS} == "ON" ]]; then
+
+    CMAKE_PARAMS+=(
+        # CMake settings for axivion suite
+        -D CMAKE_TOOLCHAIN_FILE:FILEPATH=${AXIVION_DIR}/axivion.cmake
+    )
+
+fi
 
 cmake ${CMAKE_PARAMS[@]} -S ${SOURCE_DIR} -B ${BUILD_DIR}
 
