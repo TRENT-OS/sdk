402cf562e34f5d3366fa8b6d621229eb493f6402
Franz Schauer <franz.schauer@hensoldt-cyber.de>
2021-06-18 08:25:39 +0200

    [DEV] prepare build/clean scripts for analysis project

---
+++ libs/test/analysis/axivion/clean.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -ue
+
+#-------------------------------------------------------------------------------
+# Copyright (C) 2021, HENSOLDT Cyber GmbH
+#
+# Clean the analysis project.
+#-------------------------------------------------------------------------------
+
+# get the directory the script is located in
+SCRIPT_DIR="$(cd "$(dirname "$0")" >/dev/null 2>&1 && pwd)"
+
+# set common paths
+source ${SCRIPT_DIR}/set_common_paths
+
+
+#-------------------------------------------------------------------------------
+# Remove build dir
+#-------------------------------------------------------------------------------
+
+rm ${BUILD_DIR} -rf
