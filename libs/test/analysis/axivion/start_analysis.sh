9b67a6a53e440ab981138ca921c21717e4ea41b9
Franz Schauer <franz.schauer@hensoldt-cyber.de>
2021-07-09 15:51:33 +0200

    [DEV] align libs start-analysis script to demos

--- libs/test/analysis/axivion/start_analysis.sh
+++ libs/test/analysis/axivion/start_analysis.sh
@@ -19,8 +19,15 @@ source ${SCRIPT_DIR}/set_common_paths
 
 export ENABLE_ANALYSIS=ON
 
-export AXIVION_PROJECTNAME=LibsAnalysis
-export AXIVION_OUTFILE=${BUILD_DIR}/test/analysis/libanalysis.so
+export PROJECTNAME=LibsAnalysis
+
+# The TARGETS variable defines the CMake build targets and outfiles for multiple
+# analysis projects. Usually the targets are components of a TRENTOS system. The
+# name of the analysis project will be "PROJECTNAME_TargetName".
+declare -A TARGETS=(
+    # [TargetName]="<BuildTarget> <OutfileInBuildDir>"
+    [Main]="analysis test/analysis/libanalysis.so"
+)
 
 
 #-------------------------------------------------------------------------------
@@ -38,7 +45,7 @@ export AXIVION_DASHBOARD_URL=http://hc-axiviondashboard:9090/axivion
 LOCAL_FILESTORAGE_DIR=/home/user/filestorage
 export AXIVION_DATABASES_DIR=${LOCAL_FILESTORAGE_DIR}
 SERVER_FILESTORAGE_DIR=/var/filestorage
-export AXIVION_SOURCESERVER_GITDIR=${SERVER_FILESTORAGE_DIR}/git/${AXIVION_PROJECTNAME}.git
+export AXIVION_SOURCESERVER_GITDIR=${SERVER_FILESTORAGE_DIR}/git/${PROJECTNAME}.git
 
 # ensure local filestorage exists
 mkdir -p ${LOCAL_FILESTORAGE_DIR}
@@ -69,17 +76,21 @@ else
     export AXIVION_DASHBOARD_URL=http://localhost:9090/axivion
     export AXIVION_SOURCESERVER_GITDIR=${AXIVION_PROJECT_DIR}/.git
 
-    # check if database file already exists
-    DATABASE_FILE=${LOCAL_FILESTORAGE_DIR}/${AXIVION_PROJECTNAME}.db
+    for TARGET_NAME in "${!TARGETS[@]}"; do
 
-    if [[ ! -f "${DATABASE_FILE}" ]]; then
+        # check if database file already exists
+        DATABASE_FILE=${LOCAL_FILESTORAGE_DIR}/${PROJECTNAME}_${TARGET_NAME}.db
 
-        # create empty database
-        cidbman database create ${DATABASE_FILE}
-        # install project at dashboard server
-        dashserver install-project --dbfile ${DATABASE_FILE}
+        if [[ ! -f "${DATABASE_FILE}" ]]; then
 
-    fi
+            # create empty database
+            cidbman database create ${DATABASE_FILE}
+            # install project at dashboard server
+            dashserver install-project --dbfile ${DATABASE_FILE}
+
+        fi
+
+    done
 
 fi
 
@@ -88,11 +99,26 @@ fi
 # Do analysis
 #-------------------------------------------------------------------------------
 
-# do "clean before"
+# do "clean before" for the first build
 export AXIVION_CLEAN_BEFORE=true
 
-# run axivion
-axivion_ci -j
+for TARGET_NAME in "${!TARGETS[@]}"; do
+
+    # set project name for target
+    export AXIVION_PROJECTNAME=${PROJECTNAME}_${TARGET_NAME}
+
+    # create target arguments list and set environment variables
+    read -a TARGET_ARGS <<< ${TARGETS[${TARGET_NAME}]}
+    export BUILD_TARGET=${TARGET_ARGS[0]}
+    export AXIVION_OUTFILE=${BUILD_DIR}/${TARGET_ARGS[1]}
+
+    # run axivion
+    axivion_ci -j
+
+    # skip "clean before" after the first build
+    export AXIVION_CLEAN_BEFORE=false
+
+done
 
 
 #-------------------------------------------------------------------------------
